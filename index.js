ce0ab03da19d7d4955641c2b9fc5be9673b1b82d
todoseguridadsm-cmyk <todoseguridadsm@gmail.com>
2026-04-06 18:47:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -87,7 +87,11 @@ const waClient = new Client({
       '--disable-gpu'
     ] 
   },
-  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  }
 });
 
 waClient.on('qr', (qr) => {
