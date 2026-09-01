3298c6483abda2c5225fe0d576b1850bcfa1b127
Matias Brandi <matias.brandi@gmail.com>
2026-09-01 01:41:41 -0300

    Fix: Use regex /.*/ for SPA catch-all route

--- backend/index.js
+++ backend/index.js
@@ -5868,7 +5868,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
